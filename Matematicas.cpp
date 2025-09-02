a3f84ae19b3c0dff7288d8e67911347f9c77ba0f
ruiJinno <earestrepod@javerianacali.edu.co>
2025-09-02 13:24:38 -0500

    Commit realizado por Elian Alejandro Restrepo Diaz

---
+++ Matematicas.cpp
@@ -0,0 +1,20 @@
+// Matematicas.cpp
+#include "Matematicas.h"
+
+
+// --- Definiciones (El "cómo") ---
+int sumar(int a, int b) {
+    return a + b;
+}
+
+int restar(int a, int b) {
+    return a - b;
+}
+
+int multiplicar(int a, int b) {
+    return a * b;
+}
+
+int dividir(int a, int b) {
+    return a / b;
+}
